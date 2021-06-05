86f6acb354ad31f1caf09a24a80c38ab40b0716f
arieftb22 <arieftb22@gmail.com>
2021-06-05 22:32:32 +0700

    Chore(DataUI) : Add Data.kt

--- app/src/main/java/id/my/arieftb/meowvie/persentation/model/Data.kt
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/model/Data.kt
@@ -1,10 +1,9 @@
 package id.my.arieftb.meowvie.persentation.model
 
-class Data<D>(var status: Status, var data: Data<D>?, var errorMessage: String? = null)
+data class Data<D>(var status: Status, var data: D? =null, var errorMessage: String? = null)
 
 enum class Status {
     SUCCESS,
     ERROR,
     LOADING
 }
-
