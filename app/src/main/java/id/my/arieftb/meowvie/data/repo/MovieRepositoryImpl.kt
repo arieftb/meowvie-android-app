27d2c5ef72f76dccf67c0b338c96235cf329f6f5
arieftb22 <arieftb22@gmail.com>
2021-06-05 22:28:51 +0700

    Fix(MovieRepository) : Change wrong Result class

--- app/src/main/java/id/my/arieftb/meowvie/data/repo/MovieRepositoryImpl.kt
+++ app/src/main/java/id/my/arieftb/meowvie/data/repo/MovieRepositoryImpl.kt
@@ -5,6 +5,7 @@ import id.my.arieftb.meowvie.data.remote.movie.MovieRemoteDataSource
 import id.my.arieftb.meowvie.domain.model.movie.Movie
 import id.my.arieftb.meowvie.domain.repo.MovieRepository
 import javax.inject.Inject
+import id.my.arieftb.meowvie.domain.model.Result
 
 class MovieRepositoryImpl @Inject constructor(val remote: MovieRemoteDataSource) : MovieRepository {
     override suspend fun fetchAll(request: MovieRequest, data: Movie): Result<List<Movie>> {
@@ -12,13 +13,13 @@ class MovieRepositoryImpl @Inject constructor(val remote: MovieRemoteDataSource)
         if (response.isSuccessful) {
             if (response.code() == 200) {
                 if (response.body()?.movieResults != null) {
-                    return Result.success(response.body()?.movieResults?.map {
+                    return Result.Success(data = response.body()?.movieResults?.map {
                         data.mapFromResponse(it)
-                    }!!)
+                    }?.toList()!!)
                 }
             }
-            return Result.failure(Exception("${response.code()}"))
+            return Result.Failure(Exception("${response.code()}"))
         }
-        return Result.failure(Exception(response.message()))
+        return Result.Failure(Exception("${response.code()}"))
     }
 }
