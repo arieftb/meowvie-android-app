3c10e9fd0f0e6f3c5393d9e1ce0a1ebb37fe2436
arieftb22 <arieftb22@gmail.com>
2021-06-05 22:41:35 +0700

    Refactor(Retrofit) : Add Loggin Intercepto

--- app/src/main/java/id/my/arieftb/meowvie/data/di/RetrofitModule.kt
+++ app/src/main/java/id/my/arieftb/meowvie/data/di/RetrofitModule.kt
@@ -6,6 +6,8 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import id.my.arieftb.meowvie.BuildConfig
 import id.my.arieftb.meowvie.data.remote.movie.MovieApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -15,8 +17,21 @@ import javax.inject.Singleton
 object RetrofitModule {
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        if (BuildConfig.DEBUG) level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(httpLoggingInterceptor: HttpLoggingInterceptor) = OkHttpClient.Builder()
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
         .baseUrl(BuildConfig.BASE_URL)
         .build()
 
