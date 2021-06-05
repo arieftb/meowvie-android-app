b52f0a291fb6514620fc56c1a414c28fcd10f6a9
arieftb22 <arieftb22@gmail.com>
2021-06-05 14:33:19 +0700

    Chore(Movie) : Implement Mapper
    What for?
    - Mapping From Response to This Object Data

---
+++ app/src/main/java/id/my/arieftb/meowvie/domain/model/movie/Movie.kt
@@ -0,0 +1,24 @@
+package id.my.arieftb.meowvie.domain.model.movie
+
+import id.my.arieftb.meowvie.data.model.response.movies.MovieResult
+import id.my.arieftb.meowvie.domain.base.BaseMapper
+
+class Movie(
+    var id: Long? = -1,
+    var title: String? = "",
+    var bannerPath: String? = null,
+    var posterPath: String? = null,
+    var releaseDate: String? = ""
+): MovieMapper {
+    override fun mapFromResponse(response: MovieResult?): Movie {
+        return Movie().apply {
+            id = response?.id?.toLong()
+            title = response?.originalTitle
+            bannerPath = response?.backdropPath
+            posterPath = response?.posterPath
+            releaseDate = response?.releaseDate
+        }
+    }
+}
+
+interface MovieMapper: BaseMapper.ResponseDataMapper<MovieResult, Movie>
