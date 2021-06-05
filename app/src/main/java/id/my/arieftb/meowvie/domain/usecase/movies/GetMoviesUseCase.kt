2f6966fcb563e15ceb2d6925f1675c391fdbd175
arieftb22 <arieftb22@gmail.com>
2021-06-05 14:59:55 +0700

    Chore(RequestObject) : Add MovieRequest.kt
    What for?
    - For define and initiate param for requesting movie in data source

---
+++ app/src/main/java/id/my/arieftb/meowvie/domain/usecase/movies/GetMoviesUseCase.kt
@@ -0,0 +1,12 @@
+package id.my.arieftb.meowvie.domain.usecase.movies
+
+import id.my.arieftb.meowvie.domain.model.movie.Movie
+
+interface GetMoviesUseCase {
+    suspend operator fun invoke(
+        page: Int = 1,
+        sortBy: String? = "release_date.desc",
+        releaseDateLte: String? = null,
+        releaseDateGte: String? = null
+    ): Result<List<Movie>>
+}
