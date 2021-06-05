3d8df5eeaa0ac42f9abb7e6f85bda956dc61c594
arieftb22 <arieftb22@gmail.com>
2021-06-05 12:43:45 +0700

    Fix(BaseRequest) : Change to be open class
    What for?
    - Abstraction so another Request Object can extend to it

--- app/src/main/java/id/my/arieftb/meowvie/data/model/request/BaseRequest.kt
+++ app/src/main/java/id/my/arieftb/meowvie/data/model/request/BaseRequest.kt
@@ -2,6 +2,6 @@ package id.my.arieftb.meowvie.data.model.request
 
 import id.my.arieftb.meowvie.BuildConfig
 
-class BaseRequest(
+open class BaseRequest(
     val apiKey: String = BuildConfig.API_KEY
 )
