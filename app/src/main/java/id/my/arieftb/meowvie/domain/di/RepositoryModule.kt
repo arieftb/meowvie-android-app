d9877e17f5d9ef152b270f30accc51301e937914
arieftb22 <arieftb22@gmail.com>
2021-06-06 01:59:43 +0700

    Refactor(Module) : change being object class

--- app/src/main/java/id/my/arieftb/meowvie/domain/di/RepositoryModule.kt
+++ app/src/main/java/id/my/arieftb/meowvie/domain/di/RepositoryModule.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class RepositoryModule {
+object RepositoryModule {
     @Provides
     @Singleton
     fun provideMovieRepository(remote: MovieRemoteDataSource): MovieRepository =
