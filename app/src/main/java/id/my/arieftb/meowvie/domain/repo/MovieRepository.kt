d8fecddb6a011b82904e95f0379e70efad1dbb6d
arieftb22 <arieftb22@gmail.com>
2021-06-05 22:29:13 +0700

    Fix(MovieRepository) : Change wrong Result class

--- app/src/main/java/id/my/arieftb/meowvie/domain/repo/MovieRepository.kt
+++ app/src/main/java/id/my/arieftb/meowvie/domain/repo/MovieRepository.kt
@@ -1,6 +1,7 @@
 package id.my.arieftb.meowvie.domain.repo
 
 import id.my.arieftb.meowvie.data.model.request.movie.MovieRequest
+import id.my.arieftb.meowvie.domain.model.Result
 import id.my.arieftb.meowvie.domain.model.movie.Movie
 
 interface MovieRepository {
