d4caada5334b6e07b15a7d5e3ead4a5d3f5e5715
arieftb22 <arieftb22@gmail.com>
2021-06-05 12:08:46 +0700

    Chore(RequestObject) : Add MovieRequest.kt
    What for?
    - For define and initiate param for requesting movie in data source

---
+++ app/src/main/java/id/my/arieftb/meowvie/data/model/request/MovieRequest.kt
@@ -0,0 +1,10 @@
+package id.my.arieftb.meowvie.data.model.request
+
+class MovieRequest(
+    var sortBy: String? = "release_date.desc",
+    var page: Int? = 1,
+    var language: String? = "en-US",
+    var releaseDateLte: String? = null,
+    var releaseDateGte: String? = null,
+    var region: String? = "ID"
+)
