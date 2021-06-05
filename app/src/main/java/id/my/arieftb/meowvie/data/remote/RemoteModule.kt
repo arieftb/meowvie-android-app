ec4738e559df54672ad1ff2ae83d5bd47c472502
arieftb22 <arieftb22@gmail.com>
2021-06-05 12:05:20 +0700

    Chore(Module) : add RemoteModule.kt
    What for?
    - Define dependency injection for API Service that using Retrofit

---
+++ app/src/main/java/id/my/arieftb/meowvie/data/remote/RemoteModule.kt
@@ -0,0 +1,25 @@
+package id.my.arieftb.meowvie.data.remote
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import id.my.arieftb.meowvie.data.remote.movie.MovieApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl("")
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideMovieApiService(retrofit: Retrofit) = retrofit.create(MovieApiService::class.java)
+}
