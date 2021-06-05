f017e6eb2122421f4d6534e86bd547b9a1a6b31e
arieftb22 <arieftb22@gmail.com>
2021-06-05 22:44:57 +0700

    Refactor(Data) : Separate the class

--- app/src/main/java/id/my/arieftb/meowvie/persentation/model/Data.kt
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/model/Data.kt
@@ -1,9 +1,3 @@
 package id.my.arieftb.meowvie.persentation.model
 
 data class Data<D>(var status: Status, var data: D? =null, var errorMessage: String? = null)
-
-enum class Status {
-    SUCCESS,
-    ERROR,
-    LOADING
-}
---
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/model/Status.kt
@@ -0,0 +1,7 @@
+package id.my.arieftb.meowvie.persentation.model
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
