dd2f411c938f5bafe08d4d39338668e7705a3678
arieftb22 <arieftb22@gmail.com>
2021-06-05 14:34:20 +0700

    Chore(Repository) : Add MovieRepository.kt
    What for?
    - Define method that can be execute for movie repository

---
+++ app/src/main/java/id/my/arieftb/meowvie/domain/repo/MovieRepository.kt
@@ -0,0 +1,8 @@
+package id.my.arieftb.meowvie.domain.repo
+
+import id.my.arieftb.meowvie.data.model.request.movie.MovieRequest
+import id.my.arieftb.meowvie.domain.model.movie.Movie
+
+interface MovieRepository {
+    suspend fun fetchAll(request: MovieRequest, data: Movie): Result<List<Movie>>
+}
