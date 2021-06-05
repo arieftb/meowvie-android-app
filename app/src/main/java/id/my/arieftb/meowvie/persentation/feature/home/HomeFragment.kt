cbda74bdcd41e22f6a984381bd302d4ae3e8ad07
arieftb22 <arieftb22@gmail.com>
2021-06-06 00:53:31 +0700

    Feat(Show Item Movie) : init adapter and show the data

--- app/src/main/java/id/my/arieftb/meowvie/persentation/feature/home/HomeFragment.kt
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/feature/home/HomeFragment.kt
@@ -1,15 +1,20 @@
 package id.my.arieftb.meowvie.persentation.feature.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
-import id.my.arieftb.meowvie.persentation.base.BaseFragment
 import id.my.arieftb.meowvie.databinding.FragmentHomeBinding
+import id.my.arieftb.meowvie.persentation.base.BaseFragment
+import id.my.arieftb.meowvie.persentation.feature.movie.MoviesPortraitRecyclerAdapter
+import id.my.arieftb.meowvie.persentation.model.Status
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+
+    lateinit var movieAdapter: MoviesPortraitRecyclerAdapter
+
     private val viewModel: HomeViewModelImpl by viewModels()
 
     override fun getViewBinding(): FragmentHomeBinding = FragmentHomeBinding.inflate(layoutInflater)
@@ -17,8 +22,28 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initView()
+        handleGetMovies()
+    }
+
+    private fun initView() {
+        movieAdapter = MoviesPortraitRecyclerAdapter(requireContext())
+    }
+
+    private fun handleGetMovies() {
         viewModel.moviesData.observe(viewLifecycleOwner, {
-            Log.d("MeowVieTAG", "onViewCreated: $it")
+            when (it.status) {
+                Status.SUCCESS -> {
+                    binding.sectionHomeNewMovie.status = it.status
+                    binding.sectionHomeNewMovie.layoutManager =
+                        LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+                    binding.sectionHomeNewMovie.adapter = movieAdapter
+                    movieAdapter.addAll(it.data)
+                }
+                Status.ERROR -> {
+                }
+                else -> binding.sectionHomeNewMovie.status = it.status
+            }
         })
 
         viewModel.getMovies(1)
