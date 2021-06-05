cc4515fa57565f7bbf44f92b40bd801f9e6dad1a
arieftb22 <arieftb22@gmail.com>
2021-06-06 01:58:23 +0700

    Feat(GetLanguageCode) : condition when use another bahasa indonesia default using english

--- app/src/main/java/id/my/arieftb/meowvie/data/repo/LanguageRepositoryImpl.kt
+++ app/src/main/java/id/my/arieftb/meowvie/data/repo/LanguageRepositoryImpl.kt
@@ -4,8 +4,11 @@ import id.my.arieftb.meowvie.data.local.language.LanguageLocalDataSource
 import id.my.arieftb.meowvie.domain.repo.LanguageRepository
 import javax.inject.Inject
 
-class LanguageRepositoryImpl @Inject constructor(val local: LanguageLocalDataSource): LanguageRepository {
+class LanguageRepositoryImpl @Inject constructor(val local: LanguageLocalDataSource) :
+    LanguageRepository {
     override suspend fun getLanguageCode(): String {
-        return local.getLanguageCode()
+        return if (local.getLanguageCode() != "in") {
+            "en"
+        } else local.getLanguageCode()
     }
 }
