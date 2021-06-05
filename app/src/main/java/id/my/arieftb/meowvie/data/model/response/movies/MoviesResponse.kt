0ca57ea04fbc1f2ee4c378599d6a1221155d03d3
arieftb22 <arieftb22@gmail.com>
2021-06-05 12:49:37 +0700

    Chore(Response) : Add MoviesResponse.kt
    What for?
    - For Define Response Object of get movies service api

---
+++ app/src/main/java/id/my/arieftb/meowvie/data/model/response/movies/MoviesResponse.kt
@@ -0,0 +1,15 @@
+package id.my.arieftb.meowvie.data.model.response.movies
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MoviesResponse(
+    @SerializedName("page")
+    val page: Int? = null,
+    @SerializedName("results")
+    val results: List<Result>? = null,
+    @SerializedName("total_pages")
+    val totalPages: Int? = null,
+    @SerializedName("total_results")
+    val totalResults: Int? = null
+)
