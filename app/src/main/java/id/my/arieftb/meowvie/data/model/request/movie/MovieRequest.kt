9b042dfd8fb3fcd055c4f515d6256cb2fae4b90f
arieftb22 <arieftb22@gmail.com>
2021-06-05 13:06:18 +0700

    Fix(MovieRequest) : remove nullable for property that required

--- app/src/main/java/id/my/arieftb/meowvie/data/model/request/movie/MovieRequest.kt
+++ app/src/main/java/id/my/arieftb/meowvie/data/model/request/movie/MovieRequest.kt
@@ -4,9 +4,9 @@ import id.my.arieftb.meowvie.data.model.request.BaseRequest
 
 class MovieRequest(
     var sortBy: String? = "release_date.desc",
-    var page: Int? = 1,
-    var language: String? = "en-US",
+    var page: Int = 1,
+    var language: String = "en-US",
     var releaseDateLte: String? = null,
     var releaseDateGte: String? = null,
-    var region: String? = "ID"
+    var region: String = "ID"
 ): BaseRequest()
