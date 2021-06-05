745050ec386b226444d8fb10cb2f1ac7d4036ac7
arieftb22 <arieftb22@gmail.com>
2021-06-05 14:58:40 +0700

    Chore(UseCase) : Add GetMoviesUseCase.kt
    What for?
    - For define and initiate param for requesting movie in data source

---
+++ app/src/main/java/id/my/arieftb/meowvie/domain/usecase/movies/GetMoviesUseCaseImpl.kt
@@ -0,0 +1,25 @@
+package id.my.arieftb.meowvie.domain.usecase.movies
+
+import id.my.arieftb.meowvie.data.model.request.movie.MovieRequest
+import id.my.arieftb.meowvie.domain.model.movie.Movie
+import id.my.arieftb.meowvie.domain.repo.MovieRepository
+import javax.inject.Inject
+
+class GetMoviesUseCaseImpl @Inject constructor(private val repository: MovieRepository) :
+    GetMoviesUseCase {
+    override suspend fun invoke(
+        page: Int,
+        sortBy: String?,
+        releaseDateLte: String?,
+        releaseDateGte: String?
+    ): Result<List<Movie>> {
+        val request = MovieRequest().apply {
+            this.page = page
+            this.sortBy = sortBy
+            this.releaseDateLte = releaseDateLte
+            this.releaseDateGte = releaseDateGte
+        }
+        val data = Movie()
+        return repository.fetchAll(request, data)
+    }
+}
