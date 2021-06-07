a78b8099fa02bbf02950195195726ee893ef9dcd
arieftb22 <arieftb22@gmail.com>
2021-06-08 00:02:34 +0700

    Refactor(GetMoviesUpcomingHighlight) : Change Item to 6

--- app/src/main/java/id/my/arieftb/meowvie/domain/usecase/movies/GetMoviesUpcomingHighlightUseCase.kt
+++ app/src/main/java/id/my/arieftb/meowvie/domain/usecase/movies/GetMoviesUpcomingHighlightUseCase.kt
@@ -4,5 +4,5 @@ import id.my.arieftb.meowvie.domain.model.Result
 import id.my.arieftb.meowvie.domain.model.movie.Movie
 
 interface GetMoviesUpcomingHighlightUseCase {
-    suspend fun invoke(limit: Int = 4): Result<List<Movie>>
+    suspend fun invoke(limit: Int = 6): Result<List<Movie>>
 }
