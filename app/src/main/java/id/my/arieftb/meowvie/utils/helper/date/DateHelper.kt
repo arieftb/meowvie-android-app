cebc8a2df519c5b6d5665247fbb063b375de6e7e
arieftb22 <arieftb22@gmail.com>
2021-06-07 21:01:30 +0700

    Feat(DateHelper) : helper for get or convert a datetime

---
+++ app/src/main/java/id/my/arieftb/meowvie/utils/helper/date/DateHelper.kt
@@ -0,0 +1,40 @@
+package id.my.arieftb.meowvie.utils.helper.date
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DateHelper {
+
+    private var dateTime: Date? = null
+    private var dateTimeFormat: SimpleDateFormat? = null
+
+    fun now(): DateHelper {
+        dateTime = Calendar.getInstance().time
+        return this
+    }
+
+    fun toPattern(dateTimePattern: String): DateHelper {
+        dateTimeFormat = SimpleDateFormat(dateTimePattern, Locale.getDefault())
+        return this
+    }
+
+    fun getString(): String? {
+        return dateTimeFormat?.format(dateTime)
+    }
+
+    companion object {
+        var instance: DateHelper? = null
+
+        fun instance(): DateHelper? {
+            if (instance == null) {
+                synchronized(DateHelper::class.java) {
+                    if (instance == null) {
+                        instance = DateHelper()
+                        return instance
+                    }
+                }
+            }
+            return instance
+        }
+    }
+}
