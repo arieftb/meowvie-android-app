1160cd5c2d22135b5171e67bb31b661f99bd0f5a
arieftb22 <arieftb22@gmail.com>
2021-06-09 05:29:44 +0700

    Refactor(getMoviesPopular) : Add Page Param in GetMoviesPopularUseCase

--- app/src/main/java/id/my/arieftb/meowvie/domain/usecase/movies/GetMoviesPopularUseCaseImpl.kt
+++ app/src/main/java/id/my/arieftb/meowvie/domain/usecase/movies/GetMoviesPopularUseCaseImpl.kt
@@ -8,8 +8,9 @@ class GetMoviesPopularUseCaseImpl constructor(
     private val getCurrentDateUseCase: GetCurrentDateUseCase,
     private val getMoviesUseCase: GetMoviesUseCase
 ) : GetMoviesPopularUseCase {
-    override suspend fun invoke(): Result<List<Movie>> {
+    override suspend fun invoke(page: Int): Result<List<Movie>> {
         return getMoviesUseCase.invoke(
+            page = page,
             sortBy = "popularity.desc",
             releaseDateLte = getCurrentDateUseCase.invoke("yyyy-MM-dd")
         )
