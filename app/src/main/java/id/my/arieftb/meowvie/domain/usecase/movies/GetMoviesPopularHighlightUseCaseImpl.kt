482551d6a064ef9de00b5d766be051a4ac3c6d39
arieftb22 <arieftb22@gmail.com>
2021-06-09 05:36:32 +0700

    Feat(getMoviesPopularHighlight) : Add GetMoviesPopularHighlightUseCase

---
+++ app/src/main/java/id/my/arieftb/meowvie/domain/usecase/movies/GetMoviesPopularHighlightUseCaseImpl.kt
@@ -0,0 +1,17 @@
+package id.my.arieftb.meowvie.domain.usecase.movies
+
+import id.my.arieftb.meowvie.domain.model.Result
+import id.my.arieftb.meowvie.domain.model.movie.Movie
+
+class GetMoviesPopularHighlightUseCaseImpl constructor(
+    private val getMoviesPopularUseCase: GetMoviesPopularUseCase
+) : GetMoviesPopularHighlightUseCase {
+    override suspend fun invoke(limit: Int): Result<List<Movie>> {
+        return when (val result = getMoviesPopularUseCase.invoke()) {
+            is Result.Success -> {
+                Result.Success(data = result.data.take(limit))
+            }
+            else -> result
+        }
+    }
+}
