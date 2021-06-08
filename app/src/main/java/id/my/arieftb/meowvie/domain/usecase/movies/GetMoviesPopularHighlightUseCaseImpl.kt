d3a7b34437210970bb823afe4c9b30225e0edf18
arieftb22 <arieftb22@gmail.com>
2021-06-09 05:40:03 +0700

    Refactor(UseCase DI) : inject getMoviesPopularUseCase in GetMoviesPopularHighlightUseCaseImpl.kt

--- app/src/main/java/id/my/arieftb/meowvie/domain/usecase/movies/GetMoviesPopularHighlightUseCaseImpl.kt
+++ app/src/main/java/id/my/arieftb/meowvie/domain/usecase/movies/GetMoviesPopularHighlightUseCaseImpl.kt
@@ -2,8 +2,9 @@ package id.my.arieftb.meowvie.domain.usecase.movies
 
 import id.my.arieftb.meowvie.domain.model.Result
 import id.my.arieftb.meowvie.domain.model.movie.Movie
+import javax.inject.Inject
 
-class GetMoviesPopularHighlightUseCaseImpl constructor(
+class GetMoviesPopularHighlightUseCaseImpl @Inject constructor(
     private val getMoviesPopularUseCase: GetMoviesPopularUseCase
 ) : GetMoviesPopularHighlightUseCase {
     override suspend fun invoke(limit: Int): Result<List<Movie>> {
