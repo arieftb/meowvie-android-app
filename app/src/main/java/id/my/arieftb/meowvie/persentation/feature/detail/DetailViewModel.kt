b795f36f7a90e05e5f98f4c2c4add33a0bdb8ad2
arieftb22 <arieftb22@gmail.com>
2021-06-12 22:40:09 +0700

    Feat(ContentDetail) : Add movieDetail and getDetail method in DetailViewModel.kt

--- app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailViewModel.kt
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailViewModel.kt
@@ -1,7 +1,13 @@
 package id.my.arieftb.meowvie.persentation.feature.detail
 
+import androidx.lifecycle.LiveData
 import id.my.arieftb.meowvie.constant.ContentType
+import id.my.arieftb.meowvie.domain.model.movie.MovieDetail
+import id.my.arieftb.meowvie.persentation.model.Data
 
 interface DetailViewModel {
-    fun getDetail(id: String, type: ContentType)
+    var movieData: LiveData<Data<MovieDetail>>
+
+    fun getDetail(id: Long, type: ContentType)
+    fun getMovieDetail(id: Long)
 }
