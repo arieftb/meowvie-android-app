79b67a26a569c34527521ab2f866e4c953a3feda
arieftb22 <arieftb22@gmail.com>
2021-06-12 22:40:31 +0700

    Feat(ContentDetail) : implement movieDetail and getDetail method in DetailViewModelImpl.kt

--- app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailViewModelImpl.kt
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailViewModelImpl.kt
@@ -1,13 +1,24 @@
 package id.my.arieftb.meowvie.persentation.feature.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
 import id.my.arieftb.meowvie.constant.ContentType
+import id.my.arieftb.meowvie.domain.model.movie.MovieDetail
+import id.my.arieftb.meowvie.persentation.model.Data
 
-class DetailViewModelImpl: ViewModel(), DetailViewModel {
+class DetailViewModelImpl : ViewModel(), DetailViewModel {
+    override var movieData: LiveData<Data<MovieDetail>> = MutableLiveData()
 
+    override fun getDetail(id: Long, type: ContentType) {
+        when (type) {
+            ContentType.TV_SHOW -> {
+            }
+            else -> getMovieDetail(id)
+        }
+    }
 
-    override fun getDetail(id: String, type: ContentType) {
+    override fun getMovieDetail(id: Long) {
 
     }
 }
