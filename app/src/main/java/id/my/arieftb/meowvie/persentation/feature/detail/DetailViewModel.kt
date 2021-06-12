0db1cf5723cf9a7ec5782e52250ce9a8572e36b8
arieftb22 <arieftb22@gmail.com>
2021-06-12 23:19:09 +0700

    Refactor(DataType) : change data type of movieData to MutableLiveData in DetailViewModel.kt

--- app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailViewModel.kt
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailViewModel.kt
@@ -1,12 +1,12 @@
 package id.my.arieftb.meowvie.persentation.feature.detail
 
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import id.my.arieftb.meowvie.constant.ContentType
 import id.my.arieftb.meowvie.domain.model.movie.MovieDetail
 import id.my.arieftb.meowvie.persentation.model.Data
 
 interface DetailViewModel {
-    var movieData: LiveData<Data<MovieDetail>>
+    val movieData: MutableLiveData<Data<MovieDetail>>
 
     fun getDetail(id: Long, type: ContentType)
     fun getMovieDetail(id: Long)
