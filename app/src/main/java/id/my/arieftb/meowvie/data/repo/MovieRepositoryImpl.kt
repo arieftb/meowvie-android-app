6aba3cae0ecdbce710e49bb4172bfed6c2485e95
arieftb22 <arieftb22@gmail.com>
2021-06-12 20:54:17 +0700

    Feat(ContentDetail) : implement fetch movie object in MovieRepositoryImpl.kt

--- app/src/main/java/id/my/arieftb/meowvie/data/repo/MovieRepositoryImpl.kt
+++ app/src/main/java/id/my/arieftb/meowvie/data/repo/MovieRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package id.my.arieftb.meowvie.data.repo
 
+import id.my.arieftb.meowvie.data.model.request.detail.DetailRequest
 import id.my.arieftb.meowvie.data.model.request.discover.DiscoverRequest
 import id.my.arieftb.meowvie.data.remote.movie.MovieRemoteDataSource
 import id.my.arieftb.meowvie.domain.model.Result
 import id.my.arieftb.meowvie.domain.model.movie.Movie
+import id.my.arieftb.meowvie.domain.model.movie.MovieDetail
 import id.my.arieftb.meowvie.domain.repo.MovieRepository
 import javax.inject.Inject
 
@@ -16,6 +18,20 @@ class MovieRepositoryImpl @Inject constructor(val remote: MovieRemoteDataSource)
                     return Result.Success(data = response.body()?.movieResults?.map {
                         data.mapFromResponse(it)
                     }?.toList()!!)
+                }
+                return Result.Failure(Exception("404"))
+            }
+            return Result.Failure(Exception("${response.code()}"))
+        }
+        return Result.Failure(Exception("${response.code()}"))
+    }
+
+    override suspend fun fetch(request: DetailRequest, data: MovieDetail): Result<MovieDetail> {
+        val response = remote.fetch(request)
+        if (response.isSuccessful) {
+            if (response.code() == 200) {
+                if (response.body() != null) {
+                    return Result.Success(data = data.mapFromResponse(response.body()))
                 }
                 return Result.Failure(Exception("404"))
             }
