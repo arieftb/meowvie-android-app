4ae6dd10fad815e52de5f97b636bcd70b84d4e9b
arieftb22 <arieftb22@gmail.com>
2021-06-13 00:39:27 +0700

    Feat(getTvShowDetail) : implement method fetch for fetching object of tv show detail in TvShowRepositoryImpl.kt

--- app/src/main/java/id/my/arieftb/meowvie/data/repo/TvShowRepositoryImpl.kt
+++ app/src/main/java/id/my/arieftb/meowvie/data/repo/TvShowRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package id.my.arieftb.meowvie.data.repo
 
+import id.my.arieftb.meowvie.data.model.request.detail.DetailRequest
 import id.my.arieftb.meowvie.data.model.request.discover.DiscoverRequest
 import id.my.arieftb.meowvie.data.remote.tv_show.TvShowRemoteDataSource
 import id.my.arieftb.meowvie.domain.model.Result
 import id.my.arieftb.meowvie.domain.model.tv_show.TvShow
+import id.my.arieftb.meowvie.domain.model.tv_show.TvShowDetail
 import id.my.arieftb.meowvie.domain.repo.TvShowRepository
 
 class TvShowRepositoryImpl constructor(val remote: TvShowRemoteDataSource) : TvShowRepository {
@@ -16,6 +18,22 @@ class TvShowRepositoryImpl constructor(val remote: TvShowRemoteDataSource) : TvS
                         data = response.body()?.tvShowResults?.map {
                             data.mapFromResponse(it)
                         }?.toList()!!
+                    )
+                }
+                return Result.Failure(Exception("404"))
+            }
+            return Result.Failure(Exception("${response.code()}"))
+        }
+        return Result.Failure(Exception("${response.code()}"))
+    }
+
+    override suspend fun fetch(request: DetailRequest, data: TvShowDetail): Result<TvShowDetail> {
+        val response = remote.fetch(request)
+        if (response.isSuccessful) {
+            if (response.code() == 200) {
+                if (response.body() != null) {
+                    return Result.Success(
+                        data = data.mapFromResponse(response.body())
                     )
                 }
                 return Result.Failure(Exception("404"))
