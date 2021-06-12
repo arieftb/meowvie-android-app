67bed291438ae21844ff3b8e2924a8e4f64bb7a4
arieftb22 <arieftb22@gmail.com>
2021-06-13 00:56:26 +0700

    Feat(getTvShowDetail) : add tv show live data and method get tv show detail  in DetailViewModel.kt

--- app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailViewModel.kt
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailViewModel.kt
@@ -3,11 +3,14 @@ package id.my.arieftb.meowvie.persentation.feature.detail
 import androidx.lifecycle.MutableLiveData
 import id.my.arieftb.meowvie.constant.ContentType
 import id.my.arieftb.meowvie.domain.model.movie.MovieDetail
+import id.my.arieftb.meowvie.domain.model.tv_show.TvShowDetail
 import id.my.arieftb.meowvie.persentation.model.Data
 
 interface DetailViewModel {
     val movieData: MutableLiveData<Data<MovieDetail>>
+    val tvShowData: MutableLiveData<Data<TvShowDetail>>
 
     fun getDetail(id: Long, type: ContentType)
     fun getMovieDetail(id: Long)
+    fun getTvShowDetail(id: Long)
 }
