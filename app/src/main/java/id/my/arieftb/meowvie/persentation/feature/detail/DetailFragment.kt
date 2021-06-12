a952a5c2cb8d1032d60b0518966613a37f425b9f
arieftb22 <arieftb22@gmail.com>
2021-06-12 22:30:40 +0700

    Feat(Navigation) : Get Argument for DetailFragment.kt

--- app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailFragment.kt
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailFragment.kt
@@ -3,11 +3,15 @@ package id.my.arieftb.meowvie.persentation.feature.detail
 import android.os.Bundle
 import android.view.View
 import dagger.hilt.android.AndroidEntryPoint
+import id.my.arieftb.meowvie.constant.ContentType
 import id.my.arieftb.meowvie.databinding.FragmentDetailBinding
 import id.my.arieftb.meowvie.persentation.base.BaseFragment
 
 @AndroidEntryPoint
 class DetailFragment : BaseFragment<FragmentDetailBinding>() {
+
+    var id: Long = -1
+    var type: ContentType = ContentType.MOVIE
 
     override fun getViewBinding(): FragmentDetailBinding =
         FragmentDetailBinding.inflate(layoutInflater)
@@ -15,5 +19,13 @@ class DetailFragment : BaseFragment<FragmentDetailBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initArgs()
+    }
+
+    private fun initArgs() {
+        arguments?.let {
+            id = it.getLong("id")
+            type = it.get("type") as ContentType
+        }
     }
 }
