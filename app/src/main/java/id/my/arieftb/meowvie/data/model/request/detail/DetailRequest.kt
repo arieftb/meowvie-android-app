563c7e50fa59cb8fc2c9ad808bbd0ddbec341e17
arieftb22 <arieftb22@gmail.com>
2021-06-12 20:48:43 +0700

    Refactor(DataType) : Change data type of id and language of DetailRequest.kt to not nullable

--- app/src/main/java/id/my/arieftb/meowvie/data/model/request/detail/DetailRequest.kt
+++ app/src/main/java/id/my/arieftb/meowvie/data/model/request/detail/DetailRequest.kt
@@ -3,6 +3,6 @@ package id.my.arieftb.meowvie.data.model.request.detail
 import id.my.arieftb.meowvie.data.model.request.BaseRequest
 
 class DetailRequest(
-    var id: Long? = null,
-    var language: String? = "en-US",
+    var id: Long = -1,
+    var language: String = "en-US",
 ) : BaseRequest()
