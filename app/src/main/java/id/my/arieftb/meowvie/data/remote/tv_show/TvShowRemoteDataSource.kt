2d3dd3525c0b8a0e5da7bf62d2140a25600242f7
arieftb22 <arieftb22@gmail.com>
2021-06-13 00:31:32 +0700

    Feat(getTvShowDetail) : init method fetch for fetching object of tv show detail in TvShowRemoteDataSource.kt

--- app/src/main/java/id/my/arieftb/meowvie/data/remote/tv_show/TvShowRemoteDataSource.kt
+++ app/src/main/java/id/my/arieftb/meowvie/data/remote/tv_show/TvShowRemoteDataSource.kt
@@ -1,9 +1,12 @@
 package id.my.arieftb.meowvie.data.remote.tv_show
 
+import id.my.arieftb.meowvie.data.model.request.detail.DetailRequest
 import id.my.arieftb.meowvie.data.model.request.discover.DiscoverRequest
 import id.my.arieftb.meowvie.data.model.response.tv_shows.TvShowsResponse
+import id.my.arieftb.meowvie.data.model.response.tv_shows.detail.TvShowDetailResponse
 import retrofit2.Response
 
 interface TvShowRemoteDataSource {
     suspend fun fetchAll(request: DiscoverRequest): Response<TvShowsResponse>
+    suspend fun fetch(request: DetailRequest): Response<TvShowDetailResponse>
 }
