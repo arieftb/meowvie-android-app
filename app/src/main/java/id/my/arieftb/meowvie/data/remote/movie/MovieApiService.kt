0d4b5b7f796c88969406bc09949476e7199b269a
arieftb22 <arieftb22@gmail.com>
2021-06-12 20:42:40 +0700

    Feat(ContentDetail) : init api service inside MovieApiService.kt for get detail movie

--- app/src/main/java/id/my/arieftb/meowvie/data/remote/movie/MovieApiService.kt
+++ app/src/main/java/id/my/arieftb/meowvie/data/remote/movie/MovieApiService.kt
@@ -1,12 +1,17 @@
 package id.my.arieftb.meowvie.data.remote.movie
 
 import id.my.arieftb.meowvie.data.model.response.movies.MoviesResponse
+import id.my.arieftb.meowvie.data.model.response.movies.detail.MovieDetailResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.QueryMap
 
 @JvmSuppressWildcards
 interface MovieApiService {
     @GET("discover/movie")
     suspend fun getMovies(@QueryMap queryMap: Map<String, Any>): Response<MoviesResponse>
+
+    @GET("movie/{id}")
+    suspend fun getMovie(@Path("id") id: String, @QueryMap queryMap: Map<String, Any>): Response<MovieDetailResponse>
 }
