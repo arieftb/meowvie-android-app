9d3cae0e4c16b452c62b7c415e9ecfeefd443c14
arieftb22 <arieftb22@gmail.com>
2021-06-12 23:19:46 +0700

    Feat(MovieDetail) : Implement getMovieDetailUseCase in DetailViewModelImpl.kt

--- app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailViewModelImpl.kt
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/feature/detail/DetailViewModelImpl.kt
@@ -1,14 +1,23 @@
 package id.my.arieftb.meowvie.persentation.feature.detail
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import id.my.arieftb.meowvie.constant.ContentType
+import id.my.arieftb.meowvie.domain.model.Result
 import id.my.arieftb.meowvie.domain.model.movie.MovieDetail
+import id.my.arieftb.meowvie.domain.usecase.movies.GetMovieDetailUseCase
 import id.my.arieftb.meowvie.persentation.model.Data
+import id.my.arieftb.meowvie.persentation.model.Status
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DetailViewModelImpl : ViewModel(), DetailViewModel {
-    override var movieData: LiveData<Data<MovieDetail>> = MutableLiveData()
+@HiltViewModel
+class DetailViewModelImpl @Inject constructor(private val getMovieDetailUseCase: GetMovieDetailUseCase) :
+    ViewModel(), DetailViewModel {
+    override var movieData: MutableLiveData<Data<MovieDetail>> = MutableLiveData()
 
     override fun getDetail(id: Long, type: ContentType) {
         when (type) {
@@ -19,6 +28,16 @@ class DetailViewModelImpl : ViewModel(), DetailViewModel {
     }
 
     override fun getMovieDetail(id: Long) {
-
+        movieData.value = Data(Status.LOADING)
+        viewModelScope.launch(CoroutineExceptionHandler { _, throwable ->
+            throwable.printStackTrace()
+            movieData.value = Data(Status.ERROR, errorMessage = throwable.message)
+        }) {
+            when (val result = getMovieDetailUseCase.invoke(id)) {
+                is Result.Success -> movieData.value = Data(Status.SUCCESS, data = result.data)
+                is Result.Failure -> movieData.value =
+                    Data(Status.ERROR, errorMessage = result.exception.message)
+            }
+        }
     }
 }
