471c26c3fce42b8518dab3e279304bfe80b0e846
arieftb22 <arieftb22@gmail.com>
2021-06-12 20:53:58 +0700

    Feat(ContentDetail) : add method fetch in MovieRepository.kt for fetch detail movie

--- app/src/main/java/id/my/arieftb/meowvie/domain/repo/MovieRepository.kt
+++ app/src/main/java/id/my/arieftb/meowvie/domain/repo/MovieRepository.kt
@@ -1,9 +1,12 @@
 package id.my.arieftb.meowvie.domain.repo
 
+import id.my.arieftb.meowvie.data.model.request.detail.DetailRequest
 import id.my.arieftb.meowvie.data.model.request.discover.DiscoverRequest
 import id.my.arieftb.meowvie.domain.model.Result
 import id.my.arieftb.meowvie.domain.model.movie.Movie
+import id.my.arieftb.meowvie.domain.model.movie.MovieDetail
 
 interface MovieRepository {
     suspend fun fetchAll(request: DiscoverRequest, data: Movie): Result<List<Movie>>
+    suspend fun fetch(request: DetailRequest, data: MovieDetail): Result<MovieDetail>
 }
