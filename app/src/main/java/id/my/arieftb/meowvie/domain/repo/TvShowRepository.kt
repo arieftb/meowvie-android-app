8c357ab37b172122d215fc45505154c19cb3f408
arieftb22 <arieftb22@gmail.com>
2021-06-13 00:34:14 +0700

    Feat(getTvShowDetail) : add method fetch for fetching object of tv show detail in TvShowRepository.kt

--- app/src/main/java/id/my/arieftb/meowvie/domain/repo/TvShowRepository.kt
+++ app/src/main/java/id/my/arieftb/meowvie/domain/repo/TvShowRepository.kt
@@ -3,7 +3,9 @@ package id.my.arieftb.meowvie.domain.repo
 import id.my.arieftb.meowvie.data.model.request.discover.DiscoverRequest
 import id.my.arieftb.meowvie.domain.model.Result
 import id.my.arieftb.meowvie.domain.model.tv_show.TvShow
+import id.my.arieftb.meowvie.domain.model.tv_show.TvShowDetail
 
 interface TvShowRepository {
     suspend fun fetchAll(request: DiscoverRequest, data: TvShow): Result<List<TvShow>>
+    suspend fun fetch(request: DiscoverRequest, data: TvShowDetail): Result<TvShowDetail>
 }
