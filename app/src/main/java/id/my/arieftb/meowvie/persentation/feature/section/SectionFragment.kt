5f67b0ec122c420954079486f6a62d99d77c9e2d
arieftb22 <arieftb22@gmail.com>
2021-06-14 22:02:12 +0700

    Feat(getMoviesNewSection) : implement getMoviesUseCase in SectionFragment.kt

--- app/src/main/java/id/my/arieftb/meowvie/persentation/feature/section/SectionFragment.kt
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/feature/section/SectionFragment.kt
@@ -4,16 +4,59 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import id.my.arieftb.meowvie.R
+import id.my.arieftb.meowvie.constant.SectionType
 import id.my.arieftb.meowvie.databinding.FragmentSectionBinding
 import id.my.arieftb.meowvie.persentation.base.BaseFragment
+import id.my.arieftb.meowvie.persentation.model.Status
+import id.my.arieftb.meowvie.utils.extension.hide
+import id.my.arieftb.meowvie.utils.extension.show
 
 @AndroidEntryPoint
-class SectionFragment: BaseFragment<FragmentSectionBinding>() {
+class SectionFragment : BaseFragment<FragmentSectionBinding>() {
     private val viewModel: SectionViewModelImpl by viewModels()
+    private var page: Int = 1
+    private var type: SectionType = SectionType.NEW_MOVIE
 
-    override fun getViewBinding(): FragmentSectionBinding = FragmentSectionBinding.inflate(layoutInflater)
+    override fun getViewBinding(): FragmentSectionBinding =
+        FragmentSectionBinding.inflate(layoutInflater)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initArgs()
+        getContents()
+    }
+
+    private fun initArgs() {
+        arguments?.let {
+            type = it.get("type") as SectionType
+        }
+    }
+
+    private fun getContents() {
+        viewModel.contentData.observe(viewLifecycleOwner, {
+            when (it.status) {
+                Status.SUCCESS -> {
+                }
+                Status.ERROR -> setErrorView()
+                else -> binding.shimmerSectionDefault.show()
+            }
+        })
+
+        viewModel.getContents(page, type)
+    }
+
+    private fun setErrorView(errorMessage: String? = getString(R.string.error_message_something_went_wrong)) {
+        if (page == 1) {
+            with(binding) {
+                shimmerSectionDefault.hide()
+                listSection.hide()
+                binding.textSectionErrorMessage.apply {
+                    show()
+                    text = errorMessage
+                }
+            }
+        }
     }
 }
