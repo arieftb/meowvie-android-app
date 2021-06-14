51204959e4cb9e2a0730159845cc3d40aad7b2e4
arieftb22 <arieftb22@gmail.com>
2021-06-15 00:36:30 +0700

    Refactor(NavigationBack) : Implement Back Button

--- app/src/main/java/id/my/arieftb/meowvie/persentation/feature/main/MainActivity.kt
+++ app/src/main/java/id/my/arieftb/meowvie/persentation/feature/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package id.my.arieftb.meowvie.persentation.feature.main
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import id.my.arieftb.meowvie.R
 import id.my.arieftb.meowvie.databinding.ActivityMainBinding
 import id.my.arieftb.meowvie.persentation.base.BaseActivity
 
@@ -19,13 +23,35 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         initView()
     }
 
-    private fun initView() {
-        setupBottomNavigation()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return false
     }
 
-    private fun setupBottomNavigation() {
+    private fun initView() {
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val appBarConfig = AppBarConfiguration.Builder(
+            R.id.homeFragment,
+            R.id.exploreFragment,
+            R.id.favoriteFragment,
+            R.id.accountFragment
+        ).build()
+
         (supportFragmentManager.findFragmentById(binding.fragmentMainContainer.id) as NavHostFragment).let {
             binding.bottomNavigationMain.setupWithNavController(it.findNavController())
+            NavigationUI.setupActionBarWithNavController(
+                this@MainActivity,
+                it.findNavController(),
+                appBarConfig
+            )
         }
     }
 }
