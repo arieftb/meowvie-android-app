c147cdaee88af70a145da058d35d79e15d794581
arieftb22 <arieftb22@gmail.com>
2021-06-17 16:10:07 +0700

    Test(Helper) : Add files for helping unit testing

---
+++ app/src/test/java/id/my/arieftb/meowvie/helper/TestHelper.kt
@@ -0,0 +1,45 @@
+package id.my.arieftb.meowvie.helper
+
+import com.google.gson.Gson
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
+import retrofit2.Response
+
+object TestHelper {
+    fun <R : Any?> createDummyResponse(
+        responseFileName: String?,
+        classOf: Class<out R>
+    ): Response<R> {
+        val dummyResponse = if (responseFileName != null) {
+            Gson().fromJson<R>(FileUtils.readTestResourceFile(responseFileName), classOf)
+        } else {
+            null
+        }
+        return Response.success(dummyResponse)
+    }
+
+    fun <R : Any?> createDummyResponse(
+        responseFileName: String?, statusCode: Int = 500,
+        classOf: Class<out R>
+    ): Response<R> {
+
+        val dummyResponse = if (responseFileName != null) {
+            Gson().fromJson<R>(FileUtils.readTestResourceFile(responseFileName), classOf)
+        } else {
+            null
+        }
+
+        if (responseFileName.isNullOrEmpty()) {
+            return Response.error(
+                statusCode,
+                "Test Server Error".toResponseBody("text/plain".toMediaTypeOrNull())
+            )
+        }
+
+        //Init Response
+        return Response.error(
+            statusCode, Gson().toJson(dummyResponse)
+                .toResponseBody("application/json".toMediaTypeOrNull())
+        )
+    }
+}
